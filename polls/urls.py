79ab2f8bd1974fb0a11d23898546f148a52efd02
mholliday <holliday@email.wcu.edu>
2017-11-26 20:55:11 -0500

    Update urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,9 @@ from django.conf.urls import patterns, url
 from polls import views
 
 urlpatterns = patterns('',
+    # example: /
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
+    # ex: /polls/5/details/
     url(r'^(?P<poll_id>\d+)/detail/$', views.detail, name='detail'),
     # ex: /polls/5/results/
     url(r'^(?P<poll_id>\d+)/results/$', views.results, name='results'),
